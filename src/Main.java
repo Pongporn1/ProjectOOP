796e8fc31c8f099b8c0078654d4d69609851bb53
Pongporn1 <pongporn_seetong@cmu.ac.th>
2025-02-05 10:03:11 +0700

    gust Push03

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,44 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         String s = """
-                move down
+                move up
                 """;
         try {
-            StrategyParser parse = new StrategyParser(s);
-            Strategy strategy = parse.parse();
+            StrategyParser parse = new StrategyParser(s);/////
+            Strategy strategy = parse.parse();///////
             StringBuilder sb = new StringBuilder();
             strategy.prettyPrint(sb);
             System.out.println(sb);
             Map<String, Strategy> validMinions = new HashMap<>();
             validMinions.put("Proto", strategy);
+            //////////////////////////////////////////////////////////////////////////
             Game game = new DuelMode(validMinions);
             game.gameloop();
+            //victory checker///////////////////////////////////////////////////////////////
+            if(game.getFirstLeader().getOwnedMinions().size() < game.getSecondLeader().getOwnedMinions().size()) {
+                System.out.println("winner is " + game.getSecondLeader().getName());
+            }else if (game.getFirstLeader().getOwnedMinions().size() > game.getSecondLeader().getOwnedMinions().size()){
+                System.out.println("winner is " + game.getFirstLeader().getName());
+            }else{
+                 int sump1 = 0,sump2 = 0;
+                for(Minion n : game.getFirstLeader().getOwnedMinions()) {
+                    sump1 += n.getHealth();}
+                for(Minion n : game.getSecondLeader().getOwnedMinions()) {
+                    sump2 += n.getHealth();}
+                if(sump1 > sump2) {System.out.println("winner is " + game.getFirstLeader().getName());}
+                else if (sump1 < sump2) {System.out.println("winner is " + game.getSecondLeader().getName());}
+                else {
+                    if (game.getFirstLeader().getBudget() > game.getSecondLeader().getBudget()) {
+                        System.out.println("winner is " + game.getFirstLeader().getName());
+                    } else if (game.getFirstLeader().getBudget() < game.getSecondLeader().getBudget()) {
+                        System.out.println("winner is " + game.getSecondLeader().getName());
+                    } else {
+                        System.out.println("Draw ");
+                    }
+                }
+            }
 
+            //victory check end here//////////////////////////////////////////////////////
         } catch (Exception e) {
             System.out.println(e);
             // }
