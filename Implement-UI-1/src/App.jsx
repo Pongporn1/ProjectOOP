54627e13c2049a2fc0f81d910f62ef310d6fd347
Pongporn1 <pongporn_seetong@cmu.ac.th>
2025-02-19 03:26:39 +0700

    Update App.jsx

--- Implement-UI-1/src/App.jsx
+++ Implement-UI-1/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { START, Config } from "./Components";
+import { START, Config,THANK,MODE } from "./Components";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<START />} />
+        <Route path="/MODE" element={<MODE />} />
         <Route path="/Config" element={<Config />} />
+        <Route path="/THANK" element={<THANK />} />
       </Routes>
     </Router>
   );
