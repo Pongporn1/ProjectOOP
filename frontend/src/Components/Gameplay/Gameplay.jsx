ad8154db128f7dcb4438744a9d5f0ee5e7f3cd9f
GUSTGUSTS <141816627+GUSTGUSTS@users.noreply.github.com>
2025-03-26 19:31:43 +0700

    Delete old frontend

--- frontend/src/Components/Gameplay/Gameplay.jsx
+++
@@ -1,7 +0,0 @@
-import "./gameplay.css";
-
-const Gameplay = () => {
-  return <div>gameplay</div>;
-};
-
-export default Gameplay;
--- frontend/src/join.jsx
+++
@@ -1,9 +0,0 @@
-import { Join } from "./Components"
-
-function join() {
-  return (
-    <div><Join/></div>
-  )
-}
-
-export default join
